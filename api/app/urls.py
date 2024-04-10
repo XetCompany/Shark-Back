b0f369e3ac0b9338e4a91ea9417be4e31443a6c0
XetPy1030 <xetpy1030@mail.ru>
2024-04-11 02:39:30 +0300

    Добавить поддержку просмотра медиа

--- api/app/urls.py
+++ api/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -5,4 +7,4 @@ urlpatterns = [
     path('manufacter/', include('api.app.manufacter.urls')),
     path('customer/', include('api.app.customer.urls')),
     path('common/', include('api.app.common.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
