692d2451ca080a31ecd1a25257a80812c08ada52
XetPy1030 <xetpy1030@mail.ru>
2024-04-11 19:37:23 +0300

    ImageBase64Field

--- api/app/manufacter/product/serializers.py
+++ api/app/manufacter/product/serializers.py
@@ -1,9 +1,25 @@
+import base64
+
+from django.core.files.base import ContentFile
 from rest_framework import serializers
 
 from app.models import ProductCompany
 
 
+class ImageBase64Field(serializers.ImageField):
+    def to_internal_value(self, data):
+        image_data = data['data']
+        image_name = data['name']
+        data_file = None
+        if image_data.startswith('data:image'):
+            format, imgstr = image_data.split(';base64,')
+            data_file = ContentFile(base64.b64decode(imgstr), name=image_name)
+        return super().to_internal_value(data_file)
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    photo = ImageBase64Field(required=False)
+
     class Meta:
         model = ProductCompany
         exclude = ('company',)
